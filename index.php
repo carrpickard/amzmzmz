3eb58b882774905776be81a927e4a3d8c63a6774
carrpickard <48970631+carrpickard@users.noreply.github.com>
2019-03-29 01:43:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-
+<?php
+header("Location:https://www.amazon.com/TINYHI-Pre-Tied-Formal-Tuxedo-Adjustable/dp/B01NBWA38Q/ref=sr_1_2_sspa?crid=1X6LA2DGXVPIG&keywords=bow+tie&qid=1553274049&s=gateway&sprefix=bow+%2Caps%2C300&sr=8-2-spons&psc=1")
+?>
